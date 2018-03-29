23060f8b7802d93d15a2f980d8409bc3961b032e
zanjs <suihuasheng@gmail.com>
2018-03-29 19:10:02 +0800

    add

---
+++ config/index.js
@@ -0,0 +1,15 @@
+
+// 开发环境配置
+var development = {
+  debug: true,
+  // 官网
+  official_website: 'https://www.xiaoduyu.com',
+  // 反馈邮箱（用户无法登陆或使用异常，可以通过该邮箱联系我们）
+  feedback_email: '54sword@gmail.com',
+  // API 地址
+  api_url: 'https://api.xiaoduyu.com',
+  // api_url: 'http://192.168.1.106:3000',
+  api_verstion: 'api/v1'
+}
+
+module.exports = development
